187c7aa02103f4f2b7b110f65fc07da5ef55d2fb
willysuccess <williamsogokeifeanyichukwu1@gmail.com>
2022-04-04 08:55:34 -0700

    committed

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * _isdigit - check for digit
- * @c : argument to check
- * Return: 0 or 1
+ * _isupper - check for upper case letter
+ * @c : character to check the case
+ * Return:0 or 1
  */
 
-int _isdigit(int c)
+int _isupper(int c)
 {
-return (c >= 48 && c <= 57);
+	return (c >= 65 && c <= 90);
+
 }
